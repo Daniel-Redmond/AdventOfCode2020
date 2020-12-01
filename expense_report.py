b76c1b637dc42a03419bf22d9133eddbadab8192
unknown <daniel@pvcameras.com>
2020-12-01 18:30:25 +0000

    added further comments to find_additions method of expense_report.py to add clarity on implementation

--- expense_report.py
+++ expense_report.py
@@ -67,9 +67,40 @@ class expense_report:
 
         self.logging.info(f'Looking for {numbers_aggregated_count} numbers that add up to {total_aggregate}')
 
+        # Container for correct numbers to be returned 
         numbers = []
+
+        """
+
+        Firstly we generate a dictionary containing each value in self.report_values as a key, and the total aggregate 
+        minus the key as a value
+
+        Example: 
+        total_aggregate = 2020
+
+        input = [2000,1000,500]
+
+        reported_dict = {
+            2000:20,
+            1000:1020,
+            500:1520
+        }
+
+        If we are searching for more than 2 numbers that add up to total_aggregate, we will be looping through the keys of this dictionary,
+        and for each key, we will recursively call ourself with:
+        total_aggregate = value of key 
+        numbers_aggregate_count = numbers_aggregate_count -1
+
+        Once we are only searching for 2 numbers, we will loop through the keys of this dictionary, and checking if the value also exists in the dictionary
+        - If it does exist, then both numbers add up to total_aggregate! 
+        - If it doesn't exist, move to the next key
+
+        """
         report_dict = { int(self.report_values[i]) : total_aggregate - int( self.report_values[i] ) for i in range(0,len( self.report_values ) ) }
 
+
+        # If we are only searching for 2 numbers that add up to total_aggregate we have met our break condition
+        # We will no longer be calling ourself recursively 
         if numbers_aggregated_count == 2:
             
 
